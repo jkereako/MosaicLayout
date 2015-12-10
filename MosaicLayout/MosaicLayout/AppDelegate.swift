1460a10d2bd0dd0ae9936eab0947e782f78be5f6
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2015-12-10 12:40:57 -0500

    Initial commit: add UI layout
    In this commit, the app does nothing, but, it is in a working state.

---
+++ MosaicLayout/MosaicLayout/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MosaicLayout
+//
+//  Created by Jeffrey Kereakoglow on 12/10/15.
+//  Copyright © 2015 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+}
+
